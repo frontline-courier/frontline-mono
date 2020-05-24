b64b0967206a5c09dca2128f5bcbc15662d5bb45
Aravind Appadurai <aravin.it@gmail.com>
2020-05-24 23:37:55 +0530

    Added angular/fire

--- FrontlineCourier/admin/src/environments/environment.prod.ts
+++ FrontlineCourier/admin/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyBM65EL-d5ppnBlGOZ5MYoMKVDtb0z-lKI',
+    authDomain: 'varun-enterprises.firebaseapp.com',
+    databaseURL: 'https://varun-enterprises.firebaseio.com',
+    projectId: 'varun-enterprises',
+    storageBucket: 'varun-enterprises.appspot.com',
+    messagingSenderId: '627419010873',
+    appId: '1:627419010873:web:054515e9f51513b160bae8',
+    measurementId: 'G-QPTMSCRH73',
+  },
 };
