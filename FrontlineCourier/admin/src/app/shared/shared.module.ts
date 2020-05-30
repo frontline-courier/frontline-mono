b56704fc78ea622aa7a5816745fe5d36ce8671ae
Aravind Appadurai <aravin.it@gmail.com>
2020-05-30 18:43:00 +0530

    Added Angular Firebase

--- FrontlineCourier/admin/src/app/shared/shared.module.ts
+++ FrontlineCourier/admin/src/app/shared/shared.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell/shell.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
 
 const components = [
   ShellComponent,
@@ -8,6 +16,7 @@ const components = [
 
 const modules = [
   CommonModule,
+  LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatMenuModule
 ];
 
 @NgModule({
