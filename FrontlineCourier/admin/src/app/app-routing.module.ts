7b27afad59531b5e76839bafe4516e9f38fa9675
Aravind Appadurai <aravin.it@gmail.com>
2020-05-30 20:09:48 +0530

    Added pages module

--- FrontlineCourier/admin/src/app/app-routing.module.ts
+++ FrontlineCourier/admin/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './user/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- FrontlineCourier/admin/src/app/shared/shared.module.ts
+++ FrontlineCourier/admin/src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { RouterModule } from '@angular/router';
 
@@ -20,6 +21,7 @@ const modules = [
   CommonModule,
   RouterModule,
   LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatMenuModule
+  , MatSnackBarModule
 ];
 
 @NgModule({
