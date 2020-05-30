f5689136a261f2d8b74b70fe85bfd0048e2f7b3d
Aravind Appadurai <aravin.it@gmail.com>
2020-05-30 18:45:40 +0530

    Dashboard Page

--- FrontlineCourier/admin/src/app/app.module.ts
+++ FrontlineCourier/admin/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
