d0f7ac20c097505d565418d66c530507ac011e02
Aravin <aravin.it@gmail.com>
2021-11-21 21:38:02 +0530

    Updated Options routes

--- FrontlineCourier/admin-v2/pages/api/index.ts
+++ FrontlineCourier/admin-v2/pages/api/index.ts
@@ -13,12 +13,7 @@ import nextConnect from 'next-connect';
 const handler = nextConnect();
 
 handler.options(async (req: any, res: any) => {
-    const { method } = req;
-
-    // This will allow OPTIONS request
-    if (method === "OPTIONS") {
-        return res.status(200).send("ok");
-    }
+    return res.status(200).send("ok");
 });
 
 export default handler;
