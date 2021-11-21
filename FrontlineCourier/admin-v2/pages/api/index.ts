e9ad5076bd4e07ae76e1bdd11fcbcee4cb33c537
Aravin <aravin.it@gmail.com>
2021-11-21 21:26:46 +0530

    Added option route

---
+++ FrontlineCourier/admin-v2/pages/api/index.ts
@@ -0,0 +1,24 @@
+// export default async (req, res) => {
+//     const { method } = req;
+
+//     // This will allow OPTIONS request
+//     if (method === "OPTIONS") {
+//         return res.status(200).send("ok");
+//     }
+// }
+
+
+import nextConnect from 'next-connect';
+
+const handler = nextConnect();
+
+handler.options(async (req: any, res: any) => {
+    const { method } = req;
+
+    // This will allow OPTIONS request
+    if (method === "OPTIONS") {
+        return res.status(200).send("ok");
+    }
+});
+
+export default handler;
