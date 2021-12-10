37cd44d290f7557e727f4c0ab394dcb32cc7fab9
Aravin <aravin.it@gmail.com>
2021-12-10 23:44:41 +0530

    Updated Migration Project

---
+++ FrontlineCourier/client/src/app/constants/courier-list.js
@@ -0,0 +1,586 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.courierLists = void 0;
+exports.courierLists = [
+    {
+        CourierId: 1,
+        Courier: 'DTDC Normal',
+        Description: 'Normal / Express domestic / Normal International',
+        Track: 'www.dtdc.com',
+        Status: 1
+    },
+    {
+        CourierId: 2,
+        Courier: 'FEDEX Express',
+        Description: 'Priority International',
+        Track: 'www.fedex.com',
+        Status: 1
+    },
+    {
+        CourierId: 3,
+        Courier: 'DHL Express',
+        Description: 'Express International',
+        Track: 'http://www.dhl.com/en.html',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 5,
+        Courier: 'FRONTLINE Normal',
+        Description: 'Normal courier / Same day',
+        Track: 'http://www.frontlinecourier.com',
+        Status: 1
+    },
+    {
+        CourierId: 7,
+        Courier: 'UPS Priority',
+        Description: 'Priority International',
+        Track: 'www.ups.com/in',
+        Status: 1
+    },
+    {
+        CourierId: 8,
+        Courier: 'TNT Priority',
+        Description: 'Priority International',
+        Track: 'www.tnt.com/express/en_in/site/home.html',
+        Status: 1
+    },
+    {
+        CourierId: 9,
+        Courier: 'ARAMEX Priority',
+        Description: 'Normal courier',
+        Track: 'www.aramex.com',
+        Status: 1
+    },
+    {
+        CourierId: 10,
+        Courier: 'NETWORK Normal',
+        Description: 'Network Express',
+        Track: 'https://www.networkcourier.com.sg/3DWEBSITE/index.aspx',
+        Status: 1
+    },
+    {
+        CourierId: 11,
+        Courier: 'DPEX Normal',
+        Description: 'Normal International',
+        Track: 'www.dpex.com',
+        Status: 1
+    },
+    {
+        CourierId: 12,
+        Courier: 'FRANCH Normal',
+        Description: 'Normal courier',
+        Track: 'www.franchexpress.com',
+        Status: 1
+    },
+    {
+        CourierId: 13,
+        Courier: 'BlueDart DP Express',
+        Description: 'Express Domestic',
+        Track: 'www.bluedart.com',
+        Status: 1
+    },
+    {
+        CourierId: 14,
+        Courier: 'TCI EXPRESS',
+        Description: 'Normal / Express cargo ( air / surface)',
+        Track: 'www.tciexpress.in',
+        Status: 1
+    },
+    {
+        CourierId: 15,
+        Courier: 'GATI',
+        Description: 'Normal / Express cargo ( air / surface)',
+        Track: 'www.gati.com',
+        Status: 1
+    },
+    {
+        CourierId: 17,
+        Courier: 'ST Normal',
+        Description: 'Normal courier',
+        Track: 'www.stcourier.com',
+        Status: 1
+    },
+    {
+        CourierId: 18,
+        Courier: 'LINEX',
+        Description: 'Normal International',
+        Track: 'http://www.linexsolutions.com/',
+        Status: 1
+    },
+    {
+        CourierId: 19,
+        Courier: 'PROFESSIONAL Normal',
+        Description: 'Normal / Express domestic Normal International',
+        Track: 'http://www.tpcindia.com',
+        Status: 1
+    },
+    {
+        CourierId: 20,
+        Courier: 'OVERNITE Normal',
+        Description: 'Normal courier',
+        Track: 'http://www.overnitenet.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 21,
+        Courier: 'BlueDart TDD 10.30 / 12.00',
+        Description: 'Morning delivery 10.30 / 12.00',
+        Track: 'www.bluedart.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 22,
+        Courier: 'BlueDart Critical',
+        Description: 'Passport  / Critical shipments',
+        Track: 'www.bluedart.com',
+        Status: 1
+    },
+    {
+        CourierId: 23,
+        Courier: 'BlueDart Apex',
+        Description: 'Air mode only',
+        Track: 'www.bluedart.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 24,
+        Courier: 'BlueDart Surfaceline',
+        Description: 'Surface mode only',
+        Track: 'www.bluedart.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 27,
+        Courier: 'FEDEX Priority Domestic',
+        Description: 'Priority / Economy mode domestic',
+        Track: 'www.fedex.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 28,
+        Courier: 'SHREE MAHAVIR Normal',
+        Description: 'Normal courier',
+        Track: 'http://www.shreemahavircourier.com/',
+        Status: 1
+    },
+    {
+        CourierId: 30,
+        Courier: 'ARAMEX Normal',
+        Description: 'Normal International',
+        Track: 'www.aramex.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 32,
+        Courier: 'TRACKON Normal',
+        Description: 'NULL',
+        Track: 'www.trackoncourier.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 33,
+        Courier: 'CO COURIER',
+        Description: 'NULL',
+        Track: 'NULL',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 35,
+        Courier: 'FRONTLINE Priority',
+        Description: 'NULL',
+        Track: 'http://www.frontlinecourier.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 38,
+        Courier: 'OVERNITE Priority',
+        Description: 'NULL',
+        Track: 'http://www.overnitenet.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 41,
+        Courier: 'DTDC Priority',
+        Description: 'NULL',
+        Track: 'www.dtdc.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 42,
+        Courier: 'DTDC PTP 12.00',
+        Description: 'NULL',
+        Track: 'www.dtdc.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 43,
+        Courier: 'DTDC PTP 14.00',
+        Description: 'NULL',
+        Track: 'www.dtdc.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 45,
+        Courier: 'DTDC Sunday',
+        Description: 'NULL',
+        Track: 'www.dtdc.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 46,
+        Courier: 'FRANCH Safty Plus',
+        Description: 'NULL',
+        Track: 'www.franchexpress.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 47,
+        Courier: 'TOTAL Normal',
+        Description: 'NULL',
+        Track: 'NULL',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 50,
+        Courier: 'ELITEAIRBORNE Normal',
+        Description: 'NULL',
+        Track: 'https://www.eliteairborne.com',
+        Status: 1
+    },
+    {
+        CourierId: 51,
+        Courier: 'BDHL EXPRESS',
+        Description: 'NULL',
+        Track: 'http://www.dhl.com/en.html',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 52,
+        Courier: 'OVERNITE mobile',
+        Description: 'NULL',
+        Track: 'http://www.overnitenet.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 53,
+        Courier: 'OVERNITE Passport',
+        Description: 'NULL',
+        Track: 'http://www.overnitenet.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 54,
+        Courier: 'PRIME TRACK Priority',
+        Description: 'NULL',
+        Track: 'http://www.primetrack.in/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 55,
+        Courier: 'SHREE MAHAVIR Priority',
+        Description: 'NULL',
+        Track: 'http://www.shreemahavircourier.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 56,
+        Courier: 'SPOTON Logistics',
+        Description: 'NULL',
+        Track: 'www.spoton.co.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 57,
+        Courier: 'CITY LINK Normal',
+        Description: 'NULL',
+        Track: 'http://www.citylinkexpress.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 58,
+        Courier: 'ST Priority',
+        Description: 'NULL',
+        Track: 'www.stcourier.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 59,
+        Courier: 'DHL Economy',
+        Description: 'NULL',
+        Track: 'http://www.dhl.com/en.html',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 60,
+        Courier: 'BlueDart - Apex - Fr',
+        Description: 'NULL',
+        Track: 'www.bluedart.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 61,
+        Courier: 'YOUNGKNIGHT Normal',
+        Description: 'NULL',
+        Track: 'https://youngknightexpress.com/in/tracking',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 62,
+        Courier: 'SHREE TIRUPATHI Normal',
+        Description: 'NULL',
+        Track: 'http://shreetirupaticourier.net/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 63,
+        Courier: 'BlueDart Apex Economy',
+        Description: 'NULL',
+        Track: 'www.bluedart.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 64,
+        Courier: 'Frontline Priority 2.00 Pm',
+        Description: 'NULL',
+        Track: 'NULL',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 65,
+        Courier: 'FEDEX Normal Road',
+        Description: 'NULL',
+        Track: 'www.fedex.co.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 66,
+        Courier: 'FEDEX Economy Domestic',
+        Description: 'NULL',
+        Track: 'www.fedex.co.in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 67,
+        Courier: 'Speed & Safe Courier',
+        Description: 'NULL',
+        Track: 'http://www.speedandsafe.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 68,
+        Courier: 'Atlantic International Courier',
+        Description: 'NULL',
+        Track: 'https://atlanticcourier.net',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 69,
+        Courier: 'Frontline Sunday Express',
+        Description: 'NULL',
+        Track: 'www.frontlinecourier.com',
+        Status: 1
+    },
+    {
+        CourierId: 70,
+        Courier: 'FEDEX Intl Economy',
+        Description: 'NULL',
+        Track: 'www.fedex.com/in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 71,
+        Courier: 'BDHL Priority',
+        Description: 'NULL',
+        Track: 'https://www.dhl.co.in/en',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 72,
+        Courier: 'Delhivery',
+        Description: 'www.delhivery.com',
+        Track: 'https://www.delhivery.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 73,
+        Courier: 'UPS Express',
+        Description: 'NULL',
+        Track: 'www.ups.com/in',
+        Status: 1
+    },
+    {
+        CourierId: 74,
+        Courier: 'BlueDart DP Economy',
+        Description: 'NULL',
+        Track: 'www.bluedart.com',
+        Status: 1
+    },
+    {
+        CourierId: 75,
+        Courier: 'SkyNet',
+        Description: 'NULL',
+        Track: 'https://www.skynetworldwide.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 76,
+        Courier: 'BLUEDART INTL ECONOMY',
+        Description: 'NULL',
+        Track: 'www.bluedart.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 77,
+        Courier: 'UPS Economy',
+        Description: 'NULL',
+        Track: 'www.ups.com/in',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 78,
+        Courier: 'Delhivery - Priority',
+        Description: 'NULL',
+        Track: 'https://www.delhivery.com',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 79,
+        Courier: 'Xpressbees - Normal',
+        Description: 'NULL',
+        Track: 'https://www.xpressbees.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 80,
+        Courier: 'Xpressbees - Cargo',
+        Description: 'NULL',
+        Track: 'https://www.xpressbees.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 81,
+        Courier: 'Xpressbees - Priority',
+        Description: 'NULL',
+        Track: 'https://www.xpressbees.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 82,
+        Courier: 'Rivigo Cargo',
+        Description: 'NULL',
+        Track: 'https://www.rivigo.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 83,
+        Courier: 'Rivigo Priority',
+        Description: 'NULL',
+        Track: 'https://www.rivigo.com/',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 84,
+        Courier: 'Frontline Express',
+        Description: 'NULL',
+        Track: '',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 85,
+        Courier: 'Frontline Economy',
+        Description: 'NULL',
+        Track: '',
+        Status: 'NULL'
+    },
+    {
+        CourierId: 86,
+        Courier: 'Network Express',
+        Description: 'Network Express',
+        Track: 'https://www.networkcourier.com.sg/3DWEBSITE/index.aspx',
+        Status: 1
+    },
+    {
+        CourierId: 87,
+        Courier: 'BlueDart Intl Economy',
+        Description: 'BlueDart',
+        Track: 'https://bluedart.com/home',
+        Status: 1
+    },
+    {
+        CourierId: 88,
+        Courier: 'BlueDart Intl Express',
+        Description: 'BlueDart',
+        Track: 'https://bluedart.com/home',
+        Status: 1
+    },
+    {
+        CourierId: 90,
+        Courier: 'ACX Economy',
+        Description: 'ACX Economy',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 91,
+        Courier: 'ACX Express',
+        Description: 'ACX Express',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 92,
+        Courier: 'Nandan Courier Normal',
+        Description: 'Nandan Courier Normaly',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 93,
+        Courier: 'Nandan Courier Priority',
+        Description: 'Nandan Courier Priority',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 94,
+        Courier: 'Bombino Express',
+        Description: 'Bombino Express',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 95,
+        Courier: 'Bombino Economy',
+        Description: 'Bombino Economy',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 96,
+        Courier: 'DPD Express',
+        Description: 'DPD Express',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 97,
+        Courier: 'DPD Economy',
+        Description: 'DPD Economy',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 98,
+        Courier: 'Shree Maruthi Normal',
+        Description: 'Shree Maruthi Normal',
+        Track: '',
+        Status: 1
+    },
+    {
+        CourierId: 99,
+        Courier: 'Shree Maruthi Priority',
+        Description: 'Shree Maruthi Priority',
+        Track: '',
+        Status: 1
+    },
+];
