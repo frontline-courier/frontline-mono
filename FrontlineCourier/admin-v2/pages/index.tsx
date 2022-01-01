81b3fe2df51e5f4b1892a96d648df97552e71eb6
Aravin <aravin.it@gmail.com>
2022-01-01 11:43:09 +0530

    Removed Dashboard Pages

--- FrontlineCourier/admin-v2/pages/index.tsx
+++ FrontlineCourier/admin-v2/pages/index.tsx
@@ -1,16 +1,33 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  const iframeStyle = {
+    background: '#FFFFFF;',
+    border: 'none',
+    borderRadius: '2px',
+    boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)'
+  }
+
   return (
-    <div className="flex h-screen">
-      <div className="m-auto">
-        <p className="text-2xl">Welcome to Frontline Admin Portal, Login to continue...</p>
+    <>
+      <div className="flex justify-between m-4">
+        <div className="">
+          <h2 className="text-2xl font-semibold">Dashboard</h2>
+        </div>
       </div>
-    </div>
+
+      <div className="flex m-4">
+        <div>
+          <iframe style={iframeStyle} width="640" height="480" src="https://charts.mongodb.com/charts-varun-enterprises-kjutz/embed/charts?id=f41f0174-4843-44f1-be25-7a1684f925fa&maxDataAge=3600&theme=light&autoRefresh=false"></iframe>
+        </div>
+        <div>
+          <iframe style={iframeStyle} width="640" height="480" src="https://charts.mongodb.com/charts-varun-enterprises-kjutz/embed/charts?id=69b1c5ca-3bf9-4ffe-a9a3-3ede14cfb14e&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Home
+export default withPageAuthRequired(Home);
