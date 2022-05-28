da1a445c8bd808b5a9027bc371a97c44b5493138
Aravin <aravin.it@gmail.com>
2022-05-28 23:27:38 +0530

    Updated Tailwind

--- FrontlineCourier/admin-v2/tailwind.config.js
+++ FrontlineCourier/admin-v2/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  purge: [],
+  content: [
+    './public/**/*.{html,ico,svg}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './hooks/**/*.{js,jsx,ts,tsx}',
+    './styles/**/*.css',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [
