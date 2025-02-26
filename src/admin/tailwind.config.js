ff122bae3d193608aaf52efb0e6eda827910d96f
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-02-26 23:25:58 +0530

    updated daisyui to latest

--- src/admin/tailwind.config.js
+++ src/admin/tailwind.config.js
@@ -1,18 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.{html,ico,svg}',
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './hooks/**/*.{js,jsx,ts,tsx}',
-    './styles/**/*.css',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    themes: ['light'],
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('daisyui'),
+    require('daisyui')
   ],
+  daisyui: {
+    // DaisyUI v5 specific configs
+    themes: ["light", "dark"], // specify your themes
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+  }
 }
