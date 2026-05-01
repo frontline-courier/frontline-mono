c8efd36a6b9dd45fe9eb9a7ddbaccbc2d0b15cf2
Aravind A <4869265+Aravin@users.noreply.github.com>
2026-05-01 09:48:32 +0530

    feat: Implement API authentication middleware and error handling functions

--- src/admin/pages/_app.tsx
+++ src/admin/pages/_app.tsx
@@ -2,19 +2,30 @@ import '../styles/globals.css'
 // import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css'; //styles of nprogress
 import Router from 'next/router'
 import Head from 'next/head';
 
-//Binding events. 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.remove());
-
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const handleRouteChangeStart = () => NProgress.start();
+    const handleRouteChangeComplete = () => NProgress.done();
+    const handleRouteChangeError = () => NProgress.remove();
+
+    Router.events.on('routeChangeStart', handleRouteChangeStart);
+    Router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    Router.events.on('routeChangeError', handleRouteChangeError);
+
+    return () => {
+      Router.events.off('routeChangeStart', handleRouteChangeStart);
+      Router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      Router.events.off('routeChangeError', handleRouteChangeError);
+    };
+  }, []);
+
   return (
     <UserProvider>
       <Head>
