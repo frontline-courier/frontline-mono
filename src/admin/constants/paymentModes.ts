051f3f1918d28b416cb9f325295a005bdd537431
Aravind A <4869265+Aravin@users.noreply.github.com>
2026-05-01 09:09:19 +0530

    fix: Correct order of payment modes in constants for consistency

--- src/admin/constants/paymentModes.ts
+++ src/admin/constants/paymentModes.ts
@@ -1,1 +1,1 @@
-export const paymentModes = ['PayTM', 'GPay', 'PhonePe', 'BHIM', 'NEFT', 'Cash'] as const;
+export const paymentModes = ['BHIM', 'Cash', 'GPay', 'NEFT', 'PayTM', 'PhonePe'] as const;
